f9d425ed175975d373263aa92f6f5ab031f512c1
teng <wwwsousoucom@126.com>
2020-09-27 18:34:58 +0800

    go

---
+++ web/bookstore_users_api/app/url_mappings.go
@@ -0,0 +1,12 @@
+package app
+
+import (
+	"awesome/web/bookstore_users_api/controllers/ping"
+	"awesome/web/bookstore_users_api/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	router.POST("/users", users.CreateUser)
+}
---
+++ web/bookstore_users_api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "awesome/web/bookstore_users_api/app"
+
+func main() {
+	app.StartApplication()
+}
