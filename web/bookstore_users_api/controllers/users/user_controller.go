f6d4d3dae2589d1f6793b7e0a38a31a529264588
teng <wwwsousoucom@126.com>
2020-09-27 21:34:31 +0800

    user id

--- web/bookstore_users_api/controllers/users/user_controller.go
+++ web/bookstore_users_api/controllers/users/user_controller.go
@@ -4,14 +4,13 @@ import (
 	"awesome/web/bookstore_users_api/domain/users"
 	"awesome/web/bookstore_users_api/services"
 	"awesome/web/bookstore_users_api/utils/errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	fmt.Println(user)
 	if err := c.ShouldBindJSON(&user); err != nil {
 		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
@@ -26,5 +25,17 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+	return
 }
